ed9b7afcc2945be0e6f62ce0771a355cb0633c88
Alex Silva <alexsander.barbosa@sou.inteli.edu.br>
2025-05-20 15:39:50 -0300

    fix: alteracao na modelagem fisica

--- src/migration/202505191612.sql
+++ src/migration/202505191612.sql
@@ -11,10 +11,12 @@ CREATE TABLE IF NOT EXISTS room (
 );
 
 CREATE TABLE IF NOT EXISTS bookings (
+  id SERIAL PRIMARY KEY,
   employee_id INTEGER NOT NULL,
   room_id INTEGER NOT NULL,
   dia_hora_reserva TIMESTAMP NOT NULL,
-  PRIMARY KEY (employee_id, room_id, dia_hora_reserva),
+  num_pessoas INTEGER NOT NULL,
   FOREIGN KEY (employee_id) REFERENCES employee(id),
-  FOREIGN KEY (room_id) REFERENCES room(id)
+  FOREIGN KEY (room_id) REFERENCES room(id),
+  UNIQUE (employee_id, room_id, dia_hora_reserva)
 );
