70cfd214f3047d73715c952cf31996e81e96d20d
Alex Silva <alexsander.barbosa@sou.inteli.edu.br>
2025-05-20 15:48:10 -0300

    fix: definicao e alteracao nas rotas

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
+// server.js
 const express = require('express');
-const db = require('./src/config/db');
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./src/routes/index');
 
 const app = express();
+const port = 3000;
 
-app.get('/', async (req, res) => {
-  try {
-    const result = await db.query('SELECT NOW()');
-    res.send(`Hora atual no banco: ${result.rows[0].now}`);
-  } catch (err) {
-    res.status(500).send('Erro ao conectar com o banco.');
-  }
-});
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+// Usando as rotas definidas
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
